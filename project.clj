578086a95dab470203fa79403a8f7fca05ea9216
Mike <mike@mikera.net>
2015-06-10 21:24:57 +0100

    update to snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.mikera/core.matrix.testing "0.0.4"
+(defproject net.mikera/core.matrix.testing "0.0.5-SNAPSHOT"
   :description "Testing library for core.matrix implementations"
   
   :url "https://github.com/mikera/core.matrix.testing"
