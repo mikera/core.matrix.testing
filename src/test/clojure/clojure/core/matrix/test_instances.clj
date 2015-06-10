f8f7c08794065d4503a067171ab50ce4bd79d764
Mike <mike@mikera.net>
2015-06-10 21:12:13 +0100

    NDArray instance test

--- src/test/clojure/clojure/core/matrix/test_instances.clj
+++ src/test/clojure/clojure/core/matrix/test_instances.clj
@@ -11,6 +11,7 @@
   (ctest/instance-test [[1 2] [3 4]])
   (ctest/instance-test (double-array [1 2 -3]))
   (ctest/instance-test (object-array [-1 2 -3]))
+  (ctest/instance-test (array :ndarray [-1 2 -3]))
   ;; (ctest/instance-test (array :vectorz [[1 2] [3 4]]))
   )
 
