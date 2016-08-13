182b66fce95833fd3a5ee801b4d6662781eaf9b8
mikera <mike.r.anderson.13@gmail.com>
2016-08-13 16:55:06 +0800

    Fix override warning

--- src/test/java/core/matrix/testing/CoreMatrixTest.java
+++ src/test/java/core/matrix/testing/CoreMatrixTest.java
@@ -3,6 +3,7 @@ package core.matrix.testing;
 import mikera.cljunit.ClojureTest;
 
 public class CoreMatrixTest extends ClojureTest {
+	@Override
 	public String filter() {
 		return "clojure.core.matrix";
 	}
